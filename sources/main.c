702ba970b9ba6ce2f6c13c6a2e13d00fea47a42f
David <davidisaacmijares@gmail.com>
2026-05-04 16:52:41 -0700

    Modulos: GPIO, PWM y ADC

---
+++ sources/main.c
@@ -0,0 +1,24 @@
+﻿/**
+ * @file main.c
+ * @brief 
+ * 
+ *
+ * @authors David Mijares
+ */
+
+#include <stdint.h>
+#include "PWM.h"
+#include "Sensor.h"
+
+
+int main(void)
+{ 
+
+    while(1)
+    {
+
+    }
+    return 0;  // Never reached
+}
+
+
