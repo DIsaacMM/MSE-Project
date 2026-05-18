b51e99634ea64263ee13c41225cc4790824669f9
DIsaacMM <160677502+DIsaacMM@users.noreply.github.com>
2026-05-18 08:47:53 -0700

    Adecuacionesara prueba del ESC en el main

--- sources/main.c
+++ sources/main.c
@@ -9,11 +9,12 @@
 #include <stdint.h>
 #include "PWM.h"
 #include "Sensor.h"
+#include "Timer.h" 
 
 
 // Motor Global Constants
 #define MOTOR_TIM TIM_2          // TIM that will be implemented in the main
-#define FREQUENCY 1000      // Frequency = 1kHz
+#define FREQUENCY 50      // Frequency = 1kHz
 
 // Motor 1 Global Constants
 #define MOTOR_1_PIN 0          // Pin implemented in the main 
@@ -35,6 +36,13 @@
 #define MOTOR_4_GPIO A         // PWM's GPIO that will be implemented in the main
 #define MOTOR_4_CHANNEL channel_4  // Channel that will be implemented in the main for the TIMx
 
+// Delay Global Constants
+
+#define DELAY_TIM TIM_3
+
+// ESC Global Constants
+#define ESC_MIN_DUTY 5
+#define ESC_MAX_DUTY 10
 
 
 int main(void)
@@ -48,6 +56,7 @@ int main(void)
     // Initialize Motor 1
     pwm_init(MOTOR_1_GPIO, MOTOR_TIM, MOTOR_1_PIN);
     pwm_setSignal(MOTOR_TIM, MOTOR_1_CHANNEL, FREQUENCY, duty_cycle1);
+    pwm_start(MOTOR_TIM, MOTOR_1_CHANNEL); 
 
     // Initialize Motor 2
     pwm_init(MOTOR_2_GPIO, MOTOR_TIM, MOTOR_2_PIN);
@@ -61,19 +70,22 @@ int main(void)
     pwm_init(MOTOR_4_GPIO, MOTOR_TIM, MOTOR_4_PIN);
     pwm_setSignal(MOTOR_TIM, MOTOR_4_CHANNEL, FREQUENCY, duty_cycle4);
 
-    while(1)
-    {
-        duty_cycle1 = 25;
-        duty_cycle2 = 25;
-        duty_cycle3 = 25;
-        duty_cycle4 = 25;
+    timer_init(DELAY_TIM); 
+    timer_delay_ms(DELAY_TIM, 3000);
 
-        pwm_setSignal(MOTOR_TIM, MOTOR_1_CHANNEL, FREQUENCY, duty_cycle1);
-        pwm_setSignal(MOTOR_TIM, MOTOR_2_CHANNEL, FREQUENCY, duty_cycle2);
-        pwm_setSignal(MOTOR_TIM, MOTOR_3_CHANNEL, FREQUENCY, duty_cycle3);
-        pwm_setSignal(MOTOR_TIM, MOTOR_4_CHANNEL, FREQUENCY, duty_cycle4);
-        // Small delay to prevent overwhelming the system
-        for(volatile int i = 0; i < 100; i++) { __NOP(); }
+    while (1)
+    {
+        for (uint8_t duty = ESC_MIN_DUTY; duty <= ESC_MAX_DUTY; duty++)
+        {
+            pwm_setSignal(MOTOR_TIM, MOTOR_1_CHANNEL, FREQUENCY, duty);
+            timer_delay_ms(DELAY_TIM, 500);
+        }
+
+        for (int duty = ESC_MAX_DUTY; duty >= (int)ESC_MIN_DUTY; duty--)
+        {
+            pwm_setSignal(MOTOR_TIM, MOTOR_1_CHANNEL, FREQUENCY, duty);
+            timer_delay_ms(DELAY_TIM, 500);
+        }
     }
     return 0;  // Never reached
 }
