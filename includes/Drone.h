97939cf8b849bb9db9d9099b49b20582f7b8ae6f
David <davidisaacmijares@gmail.com>
2026-05-19 15:06:44 -0700

    Modulo Drone con structs para cada drone

--- includes/Drone.h
+++ includes/Drone.h
@@ -1,7 +1,36 @@
 
-
+#ifndef DRONE_H
+#define DRONE_H
 
 #include "PWM.h"
 #include "Sensor.h"
 #include "Timer.h"
 
+typedef void (*motor_init_t)(port_t, tim_t, uint8_t);     
+typedef void (*motor_setSignal_t)(tim_t, channel_t, uint32_t, uint8_t);    
+typedef void (*motor_start_t)(tim_t, channel_t); 
+typedef void (*motor_stop_t)(tim_t, channel_t); 
+typedef struct motor
+{
+    // Motor Variables
+    uint8_t pin;
+    port_t gpio;
+    channel_t channel; 
+    tim_t tim; 
+    
+    // Motor functions
+    motor_init_t init; 
+    motor_setSignal_t setSignal; 
+    motor_start_t start; 
+    motor_stop_t stop; 
+}motor_t;
+
+// Global motors
+extern motor_t m1; 
+extern motor_t m2; 
+extern motor_t m3; 
+extern motor_t m4; 
+
+void drone_init(uint32_t freq, uint8_t esc_min_duty);
+
+#endif 
--- sources/Drone.c
+++ sources/Drone.c
@@ -2,13 +2,75 @@
 #include "Drone.h"
 
 
-// Initialize the 4 motors
+// Motors and there attributes
+    motor_t m1 = {
+        .pin = 0, 
+        .gpio = A,
+        .channel = channel_1, 
+        .tim = TIM_2, 
+        .init = pwm_init, 
+        .setSignal = pwm_setSignal, 
+        .start = pwm_start, 
+        .stop = pwm_stop
+    }; 
 
-void drone_init(tim_t motor_tim, port_t motor1_gpio, uint8_t motor1_pin, channel_t motor1_channel, )
+    motor_t m2 = {
+        .pin = 1, 
+        .gpio = A,
+        .channel = channel_2, 
+        .tim = TIM_2, 
+        .init = pwm_init, 
+        .setSignal = pwm_setSignal, 
+        .start = pwm_start, 
+        .stop = pwm_stop
+    }; 
+
+    motor_t m3 = {
+        .pin = 2, 
+        .gpio = A,
+        .channel = channel_3, 
+        .tim = TIM_2, 
+        .init = pwm_init, 
+        .setSignal = pwm_setSignal, 
+        .start = pwm_start, 
+        .stop = pwm_stop
+    }; 
+
+    motor_t m4 = {
+        .pin = 3, 
+        .gpio = A,
+        .channel = channel_4, 
+        .tim = TIM_2, 
+        .init = pwm_init, 
+        .setSignal = pwm_setSignal, 
+        .start = pwm_start, 
+        .stop = pwm_stop
+    }; 
+
+// Initialize the 4 motors
+void drone_init(uint32_t freq, uint8_t esc_min_duty)
 {
-    pwm_init(MOTOR_1_GPIO, MOTOR_TIM, MOTOR_1_PIN);
-    pwm_setSignal(MOTOR_TIM, MOTOR_1_CHANNEL, FREQUENCY, ESC_ARM_DUTY);
-    pwm_start(MOTOR_TIM, MOTOR_1_CHANNEL);
+    // Motor 1
+    m1.init(m1.gpio, m1.tim, m1.pin); 
+    m1.setSignal(m1.tim, m1.channel, freq, esc_min_duty); 
+
+    // Motor 2
+    m2.init(m2.gpio, m2.tim, m2.pin); 
+    m2.setSignal(m2.tim, m2.channel, freq, esc_min_duty); 
+
+    // Motor 3
+    m3.init(m3.gpio, m3.tim, m3.pin); 
+    m3.setSignal(m3.tim, m3.channel, freq, esc_min_duty); 
+
+    // Motor 4
+    m4.init(m4.gpio, m4.tim, m4.pin); 
+    m4.setSignal(m4.tim, m4.channel,freq, esc_min_duty); 
+
+    // Start all Motors
+    m1.start(m1.tim, m1.channel); 
+    m2.start(m2.tim, m2.channel);
+    m3.start(m3.tim, m3.channel);  
+    m4.start(m4.tim, m4.channel); 
 }
 
 void drone_up()
